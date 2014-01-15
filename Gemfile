4285633201a8b7f36a13a5f21b285dddf2afd30c
tpsingh <tpsingh@enbake.com>
2014-01-16 00:50:06 +0530

    Implemented Ace Admin Theme

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'therubyracer', platforms: :ruby
 gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 1.2'
+gem 'twitter-bootstrap-rails'
 
 group :doc do
   gem 'sdoc', require: false
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,5 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+//= require jquery-1.9.1.min
+//= require bootstrap.min
